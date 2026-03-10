6be39f58fe1e0a59535a9167ae06ade8f316b522
אלעזר גרשי <emg4706@gmail.com>
2026-03-10 12:17:38 +0200

    update

--- src/map_view.py
+++ src/map_view.py
@@ -4,17 +4,17 @@ import folium
 def sort_by_time(arr):
     pass
 
-
+# repository of colors for devices by model
 colors = ['darkgreen', 'beige', 'lightgreen', 'black', 'purple', 'darkpurple',
           'lightblue', 'orange', 'pink', 'blue', 'darkblue', 'gray', 'red', 'green']
 color_devices = {}
 
-def color_icon(model):
+def color_by_model(model):
     if model not in color_devices:
         c = colors.pop()
         color_devices[model] = c
-    icon = color_devices[model]
-    return icon
+    color_icon = color_devices[model]
+    return color_icon
 
 def create_map(images_data):
     images_gps = list(filter(lambda dicti: dicti["has_gps"] ,images_data))
@@ -24,16 +24,19 @@ def create_map(images_data):
     m = folium.Map(location=[31.5, 34.75], zoom_start=9)
 
     list_coord = []
+
     # creates markers and adds them on the map
     for dicti in images_gps:
         folium.Marker(
             location=[dicti["latitude"], dicti["longitude"]],
             tooltip="Click here for more information",
             popup=f"{dicti['filename']}<br>{dicti['datetime']}<br>{dicti['camera_model']}",
-            icon=folium.Icon(color=color_icon(dicti["camera_model"]))
+            icon=folium.Icon(color=color_by_model(dicti["camera_model"]))
         ).add_to(m)
+
         # creates list with all points
         list_coord.append([dicti["latitude"], dicti["longitude"]])
+
 
     # creates line between locations
     coord_lines = [[list_coord[i],list_coord[i+1]] for i in range(len(list_coord)-1)]
@@ -45,4 +48,5 @@ def create_map(images_data):
             opacity= '0.8'
         ).add_to(m)
 
+    # returns 'm' as string of 'html'
     return m._repr_html_()
