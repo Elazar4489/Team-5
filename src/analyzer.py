42ea210a3e73a0de4d60683ba462f34ff422269b
אלעזר גרשי <emg4706@gmail.com>
2026-03-12 00:15:57 +0200

    update analyzer.py

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,6 +1,5 @@
 from extractor import extract_all
 from pathlib import Path
-from datetime import datetime
 
 # empty new report
 def init_report():
@@ -14,24 +13,18 @@ def init_report():
     }
     return base
 
-# Conversion 'datetime' from string to 'datetime' object
-def convert_dt(data):
-    list_with_date = filter(lambda d: d['datetime'] is not None, data)
-    time_format = "%Y:%m:%d %H:%M:%S"
-    # sort the dates
-    date_list = [datetime.strptime(d['datetime'],time_format) for d in list_with_date]
-    first_time = str(min(date_list))
-    last_time = str(max(date_list))
-
-    return [first_time,last_time]
+# Sort images by time. Return list of only dicts with 'datetime'.
+def sort_images_by_time(data):
+    sorted_images = sorted(
+        [img for img in data if img["datetime"]],
+        key=lambda x: x["datetime"]
+    )
+    return sorted_images
 
 # Checks if changed devices.
 # Returns list of dicts, each one is an exchange
 def detect_camera_switches(images_data):
-    sorted_images = sorted(
-        [img for img in images_data if img["datetime"]],
-        key=lambda x: x["datetime"]
-    )
+    sorted_images = sort_images_by_time(images_data)
     switches = []
     for i in range(1, len(sorted_images)):
         prev_cam = sorted_images[i-1].get("camera_model")
@@ -60,14 +53,14 @@ def analyze(data: list[dict]) -> dict:
             report['unique_cameras'].add(d['camera_make'] + " " + d['camera_model'])
 
     # Extract the first and last datetime by conversion to datetime object
-    times =  convert_dt(data)
-    report['data_range'] = { 'start': times[0] , 'end': times[1]}
+    times =  sort_images_by_time(data)
+    report['data_range'] = { 'start': times[0]['datetime'] , 'end': times[-1]['datetime']}
 
     # Find patterns in the data
     # Switches devices
     all_switches = detect_camera_switches(data)
     if all_switches:
-        report["insights"].append(f"נמצאו {len(all_switches)} מכשירים שונים - ייתכן שהסוכן החליף מכשירים.")
+        report["insights"].append(f"נמצאו {len(report['unique_cameras'])} מכשירים שונים - ייתכן שהסוכן החליף מכשירים.")
         # Which device was changed
         for d in all_switches:
             report["insights"].append(f"ב- {d['date']} הסוכן עבר ממכשיר {d['from']} ל- {d['to']}.")
